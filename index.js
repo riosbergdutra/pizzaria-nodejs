3b275345d2b36ee5e005a31b455b85b8079f3c15
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-24 16:32:23 -0300

    adicionando diversas coisas no index

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require("express")
-const ConnectToDatabase = require("./src/database/database")
-const rota = require("./src/router/router.pedidos")
+const express = require("express");
+const ConnectToDatabase = require("./src/database/database");
+const pedido = require("./src/router/router.pedidos");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
 
-const app = express()
+const app = express();
 
-ConnectToDatabase()
-const port = 5000
+ConnectToDatabase();
+const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/rota", rota) 
+// Rota para a documentação Swagger
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/pedido", pedido);
 
 app.get("/", (req, res) => {
     res.send({
-        message: "Bem vindo ao nosso marketplace"
-    })
-})
+        message: "Bem vindo a nossa pizzaria"
+    });
+});
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
