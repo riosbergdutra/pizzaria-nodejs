0fae944a9a23d44573e2ba9cd58ee13e128711e5
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-24 16:31:27 -0300

    refactor: Melhoria a lógica das rotas de pedidos

--- src/router/router.pedidos.js
+++ src/router/router.pedidos.js
@@ -1,53 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const pedidoService = require('../service/pedido.service'); // Importe o serviço de pedidos
+const pedidoscontroller = require('../controller/pedidos.controller'); 
 
-router.get('/', async (req, res) => {
-  try {
-    const pedidos = await pedidoService.listAllPedidos();
-    res.json(pedidos);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-router.get('/:id', async (req, res) => {
-  try {
-    const pedido = await pedidoService.getPedidoById(req.params.id);
-    res.json(pedido);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+// Rota para listar todos os pedidos
+router.get('/findAll', pedidoscontroller.listAllPedidosController);
 
+// Rota para obter um pedido por ID
+router.get('/findById/:id', pedidoscontroller.getPedidoByIdController);
 
-router.post('/', async (req, res) => {
-  try {
-    const novoPedido = await pedidoService.createPedido(req.body);
-    res.status(201).json(novoPedido);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+// Rota para criar um novo pedido
+router.post('/create', pedidoscontroller.createPedidoController);
 
+// Rota para atualizar um pedido por ID
+router.put('/update/:id', pedidoscontroller.updatePedidoController);
 
-
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedPedido = await pedidoService.updatePedido(req.params.id, req.body);
-    res.json(updatedPedido);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-
-router.delete('/:id', async (req, res) => {
-  try {
-    await pedidoService.deletePedido(req.params.id);
-    res.sendStatus(204);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+// Rota para excluir um pedido por ID
+router.delete('/remove/:id', pedidoscontroller.deletePedidoController);
 
 module.exports = router;
